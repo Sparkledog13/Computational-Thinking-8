6782668325c6254d55dd745d3dfc8b0876e6d742
Sparkledog13 <quinnbrickley@seattleacademy.org>
2026-09-14 21:27:56 +0000

    im 6 years old :0

---
+++ project1.py
@@ -0,0 +1,16 @@
+print("Welcome to Trivia!!")
+print("Where we ask the hottest questions!!")
+input("")
+name = input("What is the name you would like to go by?")
+print(f"Alright {name} lets get started")
+input("")
+answer1 = input("Question one!What are two things you can never eat for breakfast")
+if answer1 == "lunch and dinner":
+    print("Correct +10 points")
+else:
+    print("Wrong that was a tough one!")  
+input("")
+print("Next question!!")
+answer2 = input("What gets wetter the more it dries?")
+if answer2 == "a towel"
+    print("Nice one +10 points")
